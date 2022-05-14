f773d567cf6a9ab79e52b9b4c58aaf0f0393d6fa
Bruce Jia <bruce.jia@autodesk.com>
2022-05-14 23:35:33 +0800

    minor

---
+++ demo_stl_pair.cpp
@@ -0,0 +1,33 @@
+#include <string>
+
+using namespace std;
+
+int main() {
+    pair<int, int> pii(1, 2);
+    pair<int, string> pis;
+    auto p = make_pair(100, "100");
+}
+
+// Question: what if a normal class and a template has the same name?
+// Answer: compilation error. It looks like definition of template class
+// has higher priority. Thus compiler raises error that the plain class has
+// problem of not providing the types.
+#ifndef _USE_TEMPLATES
+class MyClass {
+    public:
+        void foo();
+    
+    private:
+        string _somefield;
+};
+#endif
+
+#ifdef _USE_TEMPLATES
+template<typename T> class MyClass {
+    public:
+        void foo();
+
+    private:
+        int _someintfield;
+};
+#endif // _USE_TEMPLATES
--- stringview.cpp
+++ stringview.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+#include <optional>
 
 int main() {
   std::string s = "hello, world";
-  std::cout << s << std::endl;
+  std::cout << "first " << s << std::endl;
+
+  
+  std::string_view sv (s);
+  std::cout << "second " << sv << std::endl;
 
   return 0;
 }

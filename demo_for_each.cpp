680295f51d6f174a4f5f90ccf2e974a0702ccaf5
Bruce Jia <bruce.jia@autodesk.com>
2022-06-18 10:33:50 +0800

    for_each

--- demo_for_each.cpp
+++ demo_for_each.cpp
@@ -11,11 +11,21 @@ void printChar(char c) {
 int main() {
     string s = "hello, world";
 
+    // for_each returns the function (or functor) iteself.
+
+    // ignore the return value as always.
     for_each(s.begin(), s.end(), printChar);
 
-    // for_each returns the function (or functor) iteself.
+    // C-style brute force. Not a manner of civilization ...
     void * pRes = (void *)for_each(s.begin(), s.end(), printChar);
     assert(pRes == printChar);
 
+    // capture the return value with 'auto'
+    auto result = for_each(s.begin(), s.end(), printChar);
+    result('O');
+    // result("oops"); // This won't compile.
+
     return 0;
 }
+
+//TODO: try for_each with complex types and loop with functors.

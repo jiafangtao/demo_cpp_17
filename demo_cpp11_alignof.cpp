8a316ba25e9ffc5308b9338bbbc2dff310ae09ff
Bruce Jia <bruce.jia@autodesk.com>
2022-06-18 16:07:45 +0800

    Use alignas to tell the compiler to align objects

--- demo_cpp11_alignof.cpp
+++ demo_cpp11_alignof.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 
 using namespace std;
 
@@ -22,6 +24,18 @@ class Point3D : public Point2D
 public:
     double z; // gcc warning when try to get alignof z.
 };
+
+// God, please help to to align around 32 bytes.
+class alignas(32) MyPoint3DAligned
+{
+public:
+    double x;
+    double y;
+    double z;
+};
+
+template <class PointType>
+void inspect_points(int count);
 
 int main()
 {
@@ -50,13 +64,26 @@ int main()
     cout << "Alignof double: " << alignof(double) << endl;
     cout << "Alignof Point2D: " << alignof(Point2D) << endl;
     cout << "Alignof Point3D: " << alignof(Point3D) << endl;
+    cout << "Alignof MyPoint3DAligned: " << alignof(MyPoint3DAligned) << endl;
     cout << endl;
+
+    cout << "Create instances of the points and checking the address alignment ......" << endl;
+    inspect_points<Point2D>(4);
+    cout << endl;
+
+    inspect_points<MyPoint3D>(2);
+    cout << endl;
+
+    inspect_points<MyPoint3DAligned>(4);
+    cout << endl;
+
+    MyPoint3DAligned twin[2];
+    cout << "Address of the twin: " << &twin[0] << ", " << &twin[1] << endl;
+    cout << " and the distance (bytes) of twin: " << (byte *)&twin[1] - (byte *)&twin[0] << endl;
 }
 
 // See this for POD and non-POD
 //     https://docs.microsoft.com/en-us/cpp/cpp/trivial-standard-layout-and-pod-types?view=msvc-170
-
-// Question: is this a POD?
 class CoolClass
 {
 private:
@@ -71,4 +98,22 @@ void inspect_pod()
     // GCC warning because CoolClass is not a POD.
     //   warning: offset of on non-standard-layout type 'CoolClass' [-Winvalid-offsetof]
     // cout << "OffsetOf field2: " << offsetof(CoolClass, field2) << endl;
+}
+
+template <class PointType>
+void inspect_points(int count)
+{
+    vector<PointType *> pts;
+    while (count-- > 0)
+    {
+        PointType *pt = new PointType();
+        cout << "Address of the point: " /*<< hex*/ << pt << endl;
+
+        pts.push_back(pt);
+    }
+
+    for_each(pts.begin(),
+             pts.end(),
+             [](auto it)
+             { delete it; });
 }
